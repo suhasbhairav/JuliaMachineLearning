7b5677d2933ef9cbe41c18c52af0d3578adeffef
Suhas Bhairav <suhasbhairav@hotmail.com>
2015-07-17 12:50:54 +0200

    Clean Script for Azure and Latest Gen Semantics

--- src/GenerateSemantics.jl
+++ src/GenerateSemantics.jl
@@ -10,6 +10,8 @@ function get_args()
 			help = "Full path of Input File"
 		"arg3"
 			help = "Full path of the output csv"
+        "arg4"
+            help = "No of similar words. Eg:10"
 	end
 		
 	return parse_args(args)
@@ -26,31 +28,43 @@ function begin_prg()
 	#Location of output file	
 	file_output = args["arg3"]
 	
+    #No of similar words
+    no_of_similar_words = args["arg4"]
+
 	arr_length = Int[]
 	vector_model, dictionary = load_model("$model_path")
 	result_csv = open(file_output,"w")
 	fopen = open(file)	
 	f = readlines(fopen)
-	write(result_csv,"Word\tSimilar_Word\tSense_id\tSemantic_Similarity\n")
+	write(result_csv,"Word\tSrc-Sense_id\tSenseProb\tSimilar_Word\tDest_Sense_id\tSemantic_Similarity\n")
 	#dict_keys = keys(dictionary.word2id)
 	arr = Any[]
+    arr_expected_pi = Any[]
 	for x in f
 		word = strip(string((x)))
 		println(word)
 		if(haskey(dictionary.word2id,word)==true)
-			for i = 1:3				
-				values=nearest_neighbors(vector_model,dictionary,word,i,50) 
+            empty!(arr_expected_pi)
+            arr_expected_pi = expected_pi(vector_model,dictionary.word2id[word])
+			for i = 1:length(arr_expected_pi)				
+				values=nearest_neighbors(vector_model,dictionary,word,i,parseint(no_of_similar_words)) 
 				if(length(values)>0)
 					for y in values
-						empty!(arr)
-						#push!(arr,x)
-						temp_tuple = word,values
-						for z in y
-							push!(arr,string(z))
-						end
-						unshift!(arr,word)
-						write(result_csv,join(arr,"\t"),"\n")
-					println(temp_tuple)
+                        println(y)
+                        if(y[3]!=Inf && y[3]!= -Inf && y[3]!=Inf16 && y[3]!=-Inf16 && y[3]!=Inf32 && y[3]!=-Inf32)
+                            empty!(arr)
+						    #push!(arr,x)
+						    temp_tuple = word,values
+						
+                            for z in y
+							    push!(arr,string(z))
+						    end
+                            unshift!(arr,round(arr_expected_pi[i],3))
+                            unshift!(arr,i)
+						    unshift!(arr,word)
+						    write(result_csv,join(arr,"\t"),"\n")
+				        end
+                       # println(temp_tuple)
 					end
 				end
 			end				
@@ -58,6 +72,9 @@ function begin_prg()
 		
 	end
 	close(result_csv)
+   # file_sort = string($file_output,"_sort")
+     run(`sort -k 1,1 -k 2,2n -k 3,3rn -k 6,6rn $file_output`)
+    #run(`sed -i -e '1iWord\tSrc-Sense_id\tSenseProb\tSimilar_Word\tDest_Sense_id\tSemantic_Similarity\n' $file_sort`)
 end
 
 begin_prg()
